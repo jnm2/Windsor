09d7d5af2a4a00b7c1e3266fe8d86d4d8161fc95
jnm2 <me@jnm2.com>
2017-09-14 14:03:04 -0400

    Finished refactor

--- src/Castle.Windsor.Tests/Diagnostics/ConfigurationValidationExtensionsTestCase.cs
+++ src/Castle.Windsor.Tests/Diagnostics/ConfigurationValidationExtensionsTestCase.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2004-2017 Castle Project - http://www.castleproject.org/
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +59,7 @@ namespace CastleTests.Diagnostics
 				Container.Register(Component.For<Func<int, ServiceWithUnknownArg>>().AsFactory());
 			}
 
-			[Test] 
+			[Test]
 			public void Should_be_valid()
 			{
 				Assert.That(Container, HasValidComponent<ServiceWithUnknownArg>());
@@ -220,7 +220,7 @@ namespace CastleTests.Diagnostics
 				public override ConstraintResult ApplyTo<TActual>(TActual actual)
 				{
 					if (!(actual is IWindsorContainer container))
-						throw new ArgumentException("Expected an IKernel", nameof(actual));
+						throw new ArgumentException("Expected an IWindsorContainer", nameof(actual));
 
 					var unresolvables = container.GetUnresolvableDependencies();
 
