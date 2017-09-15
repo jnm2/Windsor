4731e3da44785a8ea0d663a6c6b4ff667c22c4c3
jnm2 <me@jnm2.com>
2017-09-15 14:06:55 -0400

    Added tests on expected validation results

--- src/Castle.Windsor.Tests/Diagnostics/ConfigurationValidationExtensionsTestCase.cs
+++ src/Castle.Windsor.Tests/Diagnostics/ConfigurationValidationExtensionsTestCase.cs
@@ -15,231 +15,114 @@
 namespace CastleTests.Diagnostics
 {
 	using System;
+	using System.Collections.Generic;
 	using System.Linq;
-	using System.Reflection;
 
-	using Castle.Facilities.TypedFactory;
 	using Castle.MicroKernel.Registration;
 	using Castle.Windsor;
 	using Castle.Windsor.Diagnostics;
 
 	using NUnit.Framework;
-	using NUnit.Framework.Constraints;
 
 	[TestFixture]
-	public class ConfigurationValidationExtensionsTestCase
+	public static class ConfigurationValidationExtensionsTestCase
 	{
-		public sealed class Dependency_on_service_with_unknown_arg : ContainerFixture
+		private sealed class ServiceWithUnknownArg
 		{
-			private sealed class ServiceWithUnknownArg
+			public ServiceWithUnknownArg(int arg)
 			{
-				public ServiceWithUnknownArg(int arg)
-				{
-				}
-			}
-
-			[Test]
-			public void Should_be_invalid()
-			{
-				Assert.That(Container, HasInvalidComponent<ServiceWithUnknownArg>());
 			}
 		}
 
-		public sealed class Dependency_on_service_with_unknown_arg_with_explicit_factory : ContainerFixture
+		private sealed class ServiceConsumingViaValidFactory
 		{
-			private sealed class ServiceWithUnknownArg
+			public ServiceConsumingViaValidFactory(Func<int, ServiceWithUnknownArg> factory)
 			{
-				public ServiceWithUnknownArg(int arg)
-				{
-				}
-			}
-
-			public Dependency_on_service_with_unknown_arg_with_explicit_factory()
-			{
-				Container.Register(Component.For<Func<int, ServiceWithUnknownArg>>().AsFactory());
-			}
-
-			[Test]
-			public void Should_be_valid()
-			{
-				Assert.That(Container, HasValidComponent<ServiceWithUnknownArg>());
 			}
 		}
 
-		public sealed class Dependency_on_factory_of_service_with_unknown_arg_implicit : ContainerFixture
+		private sealed class ServiceConsumingViaInvalidFactory
 		{
-			private sealed class ServiceWithUnknownArg
+			public ServiceConsumingViaInvalidFactory(Func<ServiceWithUnknownArg> factory)
 			{
-				public ServiceWithUnknownArg(int arg)
-				{
-				}
-			}
-
-			private sealed class ConsumingService
-			{
-				public ConsumingService(Func<int, ServiceWithUnknownArg> factory) { }
-			}
-
-			[Test]
-			public void Service_with_unknown_arg_should_be_valid()
-			{
-				Assert.That(Container, HasValidComponent<ServiceWithUnknownArg>());
-			}
-
-			[Test]
-			public void Consuming_service_should_be_valid()
-			{
-				Assert.That(Container, HasValidComponent<ConsumingService>());
 			}
 		}
 
-		public sealed class Dependency_on_factory_of_service_with_unknown_arg_explicit : ContainerFixture
+		private sealed class ServiceConsumingDirectly
 		{
-			private sealed class ServiceWithUnknownArg
+			public ServiceConsumingDirectly(ServiceWithUnknownArg service)
 			{
-				public ServiceWithUnknownArg(int arg)
-				{
-				}
-			}
-
-			private sealed class ConsumingService
-			{
-				public ConsumingService(Func<int, ServiceWithUnknownArg> factory)
-				{
-				}
-			}
-
-			public Dependency_on_factory_of_service_with_unknown_arg_explicit()
-			{
-				Container.Register(Component.For<Func<int, ServiceWithUnknownArg>>().AsFactory());
-			}
-
-			[Test]
-			public void Service_with_unknown_arg_should_be_valid()
-			{
-				Assert.That(Container, HasValidComponent<ServiceWithUnknownArg>());
-			}
-
-			[Test]
-			public void Consuming_service_should_be_valid()
-			{
-				Assert.That(Container, HasValidComponent<ConsumingService>());
 			}
 		}
 
-		public sealed class Dependency_on_factory_of_service_missing_unknown_arg_with_implicit_factory : ContainerFixture
+		public static IEnumerable<TestCaseData> TestCases()
 		{
-			private sealed class ServiceWithUnknownArg
-			{
-				public ServiceWithUnknownArg(int arg)
-				{
-				}
-			}
+			var bools = new[] { false, true };
 
-			private sealed class ConsumingService
-			{
-				public ConsumingService(Func<ServiceWithUnknownArg> factory) { }
-			}
+			foreach (var withValidExplicitFactory in bools)
+				foreach (var withInvalidExplicitFactory in bools)
+					foreach (var withValidImplicitFactory in bools)
+						foreach (var withInvalidImplicitFactory in bools)
+							foreach (var withDirectDependent in bools)
+							{
+								var argumentNames = new List<string>();
+								if (withValidExplicitFactory) argumentNames.Add("valid explicit factory");
+								if (withInvalidExplicitFactory) argumentNames.Add("invalid explicit factory");
+								if (withValidImplicitFactory) argumentNames.Add("valid implicit factory");
+								if (withInvalidImplicitFactory) argumentNames.Add("invalid implicit factory");
+								if (withDirectDependent) argumentNames.Add("direct dependent");
 
-			[Test]
-			public void Service_with_unknown_arg_should_be_invalid()
-			{
-				Assert.That(Container, HasInvalidComponent<ServiceWithUnknownArg>());
-			}
-
-			[Test]
-			public void Consuming_service_with_improper_factory_should_be_invalid()
-			{
-				Assert.That(Container, HasInvalidComponent<ConsumingService>());
-			}
+								yield return new TestCaseData(
+									withValidExplicitFactory,
+									withInvalidExplicitFactory,
+									withValidImplicitFactory,
+									withInvalidImplicitFactory,
+									withDirectDependent).SetName("{m}(with " + string.Join(", ", argumentNames) + ")");
+							}
 		}
 
-		public sealed class Dependency_on_factory_of_service_missing_unknown_arg_with_explicit_factory : ContainerFixture
+		[TestCaseSource(nameof(TestCases))]
+		public static void Test_serivce_with_runtime_arg(bool withValidExplicitFactory, bool withInvalidExplicitFactory, bool withValidImplicitFactory, bool withInvalidImplicitFactory, bool withDirectDependent)
 		{
-			private sealed class ServiceWithUnknownArg
+			using (var container = new WindsorContainer())
 			{
-				public ServiceWithUnknownArg(int arg)
+				container.Register(Component.For<ServiceWithUnknownArg>());
+
+				if (withValidExplicitFactory)
+					container.Register(Component.For<Func<int, ServiceWithUnknownArg>>());
+
+				if (withInvalidExplicitFactory)
+					container.Register(Component.For<Func<ServiceWithUnknownArg>>());
+
+				if (withValidImplicitFactory)
+					container.Register(Component.For<ServiceConsumingViaValidFactory>());
+
+				if (withInvalidImplicitFactory)
+					container.Register(Component.For<ServiceConsumingViaInvalidFactory>());
+
+				if (withDirectDependent)
+					container.Register(Component.For<ServiceConsumingDirectly>());
+
+
+				var validationInfo = container.GetValidationInfo().ToDictionary(_ => _.ServiceType);
+
+				Assert.Multiple(() =>
 				{
-				}
-			}
+					var serviceInfo = validationInfo[typeof(ServiceWithUnknownArg)];
 
-			private sealed class ConsumingService
-			{
-				public ConsumingService(Func<ServiceWithUnknownArg> factory)
-				{
-				}
-			}
+					Assert.That(serviceInfo, Has.Property("RuntimeParameters").With.Exactly(1).Items);
 
-			public Dependency_on_factory_of_service_missing_unknown_arg_with_explicit_factory()
-			{
-				Container.Register(Component.For<Func<int, ServiceWithUnknownArg>>().AsFactory());
-			}
+					Assert.That(serviceInfo, Has.Property("ReturnedByTypedFactories").With.Exactly(
+						(withValidExplicitFactory | withValidImplicitFactory ? 1 : 0)
+						+ (withInvalidExplicitFactory | withInvalidImplicitFactory ? 1 : 0)).Items);
 
-			[Test]
-			public void Service_with_unknown_arg_should_be_valid()
-			{
-				Assert.That(Container, HasValidComponent<ServiceWithUnknownArg>());
-			}
+					Assert.That(serviceInfo, Has.Property("TypedFactoriesLackingParameters").With.Exactly(
+						withInvalidExplicitFactory | withInvalidImplicitFactory ? 1 : 0).Items);
 
-			[Test]
-			public void Consuming_service_with_improper_factory_should_be_invalid()
-			{
-				Assert.That(Container, HasInvalidComponent<ConsumingService>());
-			}
-		}
-
-		public abstract class ContainerFixture : IDisposable
-		{
-			private readonly IWindsorContainer container;
-
-			protected IWindsorContainer Container => container;
-
-			protected ContainerFixture()
-			{
-				container = new WindsorContainer();
-				container.AddFacility<TypedFactoryFacility>();
-
-				foreach (var nestedType in GetType().GetNestedTypes(BindingFlags.Public | BindingFlags.NonPublic))
-					container.Register(Component.For(nestedType));
-			}
-
-			protected Constraint HasInvalidComponent<TComponent>() => new ComponentValidityConstraint(typeof(TComponent), shouldBeAbleToResolve: false);
-			protected Constraint HasValidComponent<TComponent>() => new ComponentValidityConstraint(typeof(TComponent), shouldBeAbleToResolve: true);
-
-			private sealed class ComponentValidityConstraint : Constraint
-			{
-				private readonly Type componentType;
-				private readonly bool shouldBeAbleToResolve;
-
-				public ComponentValidityConstraint(Type componentType, bool shouldBeAbleToResolve)
-				{
-					this.componentType = componentType;
-					this.shouldBeAbleToResolve = shouldBeAbleToResolve;
-				}
-
-				public override ConstraintResult ApplyTo<TActual>(TActual actual)
-				{
-					if (!(actual is IWindsorContainer container))
-						throw new ArgumentException("Expected an IWindsorContainer", nameof(actual));
-
-					var unresolvables = container.GetUnresolvableDependencies();
-
-					var canActuallyResolve = unresolvables.Any(_ =>
-					{
-						var handlerSupportsComponent = _.handler.Supports(componentType);
-						var handlerHasMissingDependencies = _.handler.MissingDependencies.Any();
-						return handlerSupportsComponent && !handlerHasMissingDependencies;
-					});
-
-					var isSuccess = shouldBeAbleToResolve == canActuallyResolve;
-
-					return new ConstraintResult(this, actual, isSuccess);
-				}
-			}
-
-			public void Dispose()
-			{
-				container.Dispose();
+					if (withDirectDependent)
+						Assert.That(validationInfo[typeof(ServiceConsumingDirectly)],
+							Has.Property("DirectDependenciesLackingRuntimeParameters").With.Exactly(1).Items);
+				});
 			}
 		}
 	}
